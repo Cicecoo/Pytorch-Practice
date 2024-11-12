2732a563eacbbb81e91f718720d9e41ac31a5215
张昭骏 <89625845+Cicecoo@users.noreply.github.com>
2024-11-13 00:04:44 +0800

    update: 对于通道数为C的输入，“一组”卷积核包含C个，每个卷积核只在其对应通道内计算，N组卷积核对应N维输出

--- Conv-Channel/test.py
+++ Conv-Channel/test.py
@@ -10,3 +10,12 @@ conv1 = nn.Conv2d(1, 6, 3)
 
 print(conv1.weight.shape)
 
+''' output
+torch.Size([3, 3, 3, 3])
+torch.Size([6, 1, 3, 3])
+'''
+
+# The number of kernel groups is equal to the number of output feature maps.
+# 所以实际不是由卷积核的通道数决定输出通道数
+# 而是由卷积核的“组数” —— 将单通道的kernel视作“一个卷积核”，则三通道的kernel视作“一组三个卷积核”
+# [6, 1, 3, 3]：六组，每组一个卷积核、卷积核大小3x3
